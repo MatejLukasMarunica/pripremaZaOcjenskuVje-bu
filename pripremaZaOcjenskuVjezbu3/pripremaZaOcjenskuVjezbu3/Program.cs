15c82e6c0d1ab220a3301708c0e979224eb86470
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-11-08 09:31:34 +0100

    Add files via upload

---
+++ pripremaZaOcjenskuVjezbu3/pripremaZaOcjenskuVjezbu3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaOcjenskuVjezbu3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nesto, s1, s2;
+
+            Console.WriteLine(" Unesi neku rečenicu. ");
+            nesto = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(nesto.Substring(0, nesto.IndexOf(" ")));
+            Console.Write(nesto.Substring(nesto.LastIndexOf(" ", nesto.Length -1)));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
